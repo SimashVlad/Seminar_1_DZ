a0fb9a1c1c8d2f80781392517f254623e0140314
Vladislav <SimashVlad@yandex.ru>
2022-04-27 22:43:13 +0500

    Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,4 +2,7 @@
 // принимает число и выдает, является ли 
 // число чётным (делиться ли оно без остатка).
 
-
+Console.Write("Введите число a ");
+int num = int.Parse(Console.ReadLine());
+bool result = num % 2 == 0;
+Console.WriteLine(result ? "Да" : "Нет");
